8fe97ff73c684cf382a20005025ff6938e49cdec
Madelyn <maddyac11@gmail.com>
2023-04-06 18:01:02 -0400

    On step 40 and the destroy doesn't work

--- Breakout - M.C/Assets/GameManager.cs
+++ Breakout - M.C/Assets/GameManager.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject brickPrefab;
+
+    public float xSpace;
+    public float xOffset;
     // Start is called before the first frame update
     void Start()
     {
         for (int x = 0;x < 10; x++)
         {
-            Instantiate(brickPrefab, new Vector2(x, 3), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 3), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(brickPrefab, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
         }
     }
-
-    // Update is called once per frame
-
 }
---
+++ Breakout - M.C/Assets/Score.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+            score = 0;
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = score.ToString();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
