c7d804a93f32d48f96b5627669c1643052f4b121
Madelyn <maddyac11@gmail.com>
2023-04-27 17:59:45 -0400

    On step 30, C & D Space Invaders

--- M.C - Space Invaders/Assets/GameManager.cs
+++ M.C - Space Invaders/Assets/GameManager.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public float xSpace;
+    public float xOffset;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < 10; x++)
+        {
+            Instantiate(enemy, new Vector2(x * xSpace + xOffset, 3), Quaternion.identity);
+            Instantiate(enemy, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(enemy, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
+        }
     }
-
+    
     // Update is called once per frame
     void Update()
     {
